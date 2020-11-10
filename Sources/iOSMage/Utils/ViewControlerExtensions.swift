8efa7619620b48cce4e9b37365a2a2bc018b2b97
Jefferson Puchalski <puchiruzuki@gmail.com>
2020-11-10 12:23:28 -0300

    Add ViewController extension
    This extension can instantiate nibs with his description name and self bundle
    or can instanciate with given name and bundle.

---
+++ Sources/iOSMage/Utils/ViewControlerExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jefferson Barbosa Puchalski on 10/11/20.
+//
+
+import UIKit
+
+public extension UIViewController {
+    
+    private static func genericInstance<T: UIViewController>() -> T {
+        return T.init(nibName: String(describing: self), bundle: Bundle(for: self))
+    }
+    
+    private static func genericInstance<T: UIViewController>(for nibName: String, inBundle: Bundle) -> T {
+        return T.init(nibName: nibName, bundle: inBundle)
+    }
+    
+    static func instance() -> Self {
+        return genericInstance()
+    }
+}
